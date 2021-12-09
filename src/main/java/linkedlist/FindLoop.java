3ca30cf653708ef81494255da6c7d38befbca74c
xiaolongwang <xiaolong.wang.dev@gmail.com>
2021-12-08 22:53:57 -0500

    understanding the math behind Floyd's loop detection algorithm

--- src/main/java/linkedlist/FindLoop.java
+++ src/main/java/linkedlist/FindLoop.java
@@ -19,6 +19,32 @@ public class FindLoop {
         }
     }
 
+    /**
+     * understanding Floyd's algorithm mathematically
+     * let L be loop length
+     *     M be the distance from head to entry point
+     *     N be the distance between entry point and the point of the 2 cursors first meet
+     *     q be the number of full rounds slow cursor travelled before the first meet
+     *     p be the number of full rounds fast cursor travelled before the first meet
+     * So,
+     * full distance traveled by the fast cursor is: M + pL + N
+     * full distance traveled by the slow cursor is: M + qL + N
+     * because they used the same amount of time, and fast cursor is velocity is 2 times of the slow
+     * M + pL + N = 2(M + qL + N)
+     * ->
+     * (p - 2q) L = M + N            (expression 1)
+     *
+     * let's now abstract the 2nd part of the algorithm - put 1 cursor back the head, and then they both
+     * start walking at the velocity of 1 (let's call them cursor A and B)
+     * When A has again travelled M, B has also travelled M in the loop.
+     * Remember, B is already N steps away from the loop entry point. So now its distance from the entry point is N + M.
+     * Look at expression 1, N + M equals integer multiples of the length of a full loop.
+     * This means that cursor B is now right on the entry point.
+     * If you remember, cursor A has travelled M, which is the distance from head to the entry point, so it is also
+     * right on the entry point.
+     * Therefore, cursor A and B will meet at the entry point of the loop!
+     *
+     */
     public Node findEnterPoint(Node input) {
         if(input == null) return null;
         Node slow = input;
