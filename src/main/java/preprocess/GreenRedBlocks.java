507cd724190e39101a6a102c6b1396d2d545e4cd
xiaolongwang <xiaolong.wang.dev@gmail.com>
2022-01-04 22:53:24 -0500

    preprocess: green red blocks - comments

--- src/main/java/preprocess/GreenRedBlocks.java
+++ src/main/java/preprocess/GreenRedBlocks.java
@@ -4,6 +4,7 @@ public class GreenRedBlocks {
 
     //  RGRGRGG to RRRGGGG, paint 2 times
     public int paint(char[] blocks) {
+        // when looking to the left at index i, how many blocks are green
         int[] leftGs = new int[blocks.length + 1];
         int gs = 0;
         for (int i = 0; i < blocks.length; i++) {
@@ -12,6 +13,8 @@ public class GreenRedBlocks {
         }
         leftGs[blocks.length] = gs;
 
+        // when looking to the right at index i - 1, how many blocks are red
+        // * you can think rightRs[0] is looking at index -1, so it's observing all blocks
         int[] rightRs = new int[blocks.length + 1];
         int rs = 0;
         for (int i = blocks.length - 1; i >= 0; i--) {
@@ -21,6 +24,8 @@ public class GreenRedBlocks {
         rightRs[0] = rs;
 
         int min = Integer.MAX_VALUE;
+        // assume i is the length of the target R zone
+        // getting the index right is the trickiest thing in the problem
         for (int i = 0; i < blocks.length; i++) {
             min = Math.min(min, leftGs[i] + rightRs[i]);
         }
