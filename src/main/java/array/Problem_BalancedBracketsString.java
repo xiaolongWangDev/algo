995b0b836352042921232a353174ca859962eef1
xiaolongwang <xiaolong.wang.dev@gmail.com>
2022-01-06 20:50:35 -0500

    Problem: find pairs of numbers that are of certain distance

--- src/main/java/array/Problem_BalancedBracketsString.java
+++ src/main/java/array/Problem_BalancedBracketsString.java
@@ -16,10 +16,31 @@ public class Problem_BalancedBracketsString {
         return count == 0;
     }
 
+    public int fix(char[] input) {
+        int missingLefts = 0;
+        int missingRights = 0;
+        for (char c : input) {
+            if (c == '(') {
+                missingRights++;
+            } else if (c == ')') {
+                missingRights--;
+            }
+            if (missingRights < 0) {
+                missingLefts++;
+                missingRights = 0;
+            }
+        }
+        return missingRights + missingLefts;
+    }
+
     public static void main(String[] args) {
         Problem_BalancedBracketsString p = new Problem_BalancedBracketsString();
         System.out.println(p.determine("()()()(()".toCharArray()));
         System.out.println(p.determine("()()()(())".toCharArray()));
         System.out.println(p.determine("())()(())".toCharArray()));
+
+        System.out.println(p.fix("()()())".toCharArray()));
+        System.out.println(p.fix("()()()))))".toCharArray()));
+        System.out.println(p.fix("()()()))))(()(".toCharArray()));
     }
 }
