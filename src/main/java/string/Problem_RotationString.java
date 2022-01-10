b47db9c23ecca31f31e944251eb4c7a7a869f3fa
xiaolongwang <xiaolong.wang.dev@gmail.com>
2022-01-09 21:18:33 -0500

    problem: rotation string

---
+++ src/main/java/string/Problem_RotationString.java
@@ -0,0 +1,15 @@
+package string;
+
+public class Problem_RotationString {
+    public boolean shareSameRotation(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+        KMP kmp = new KMP();
+        return kmp.findSubstring(s1 + s1, s2) > -1;
+    }
+
+    public static void main(String[] args) {
+        Problem_RotationString p = new Problem_RotationString();
+        System.out.println(p.shareSameRotation("abcde", "deabc"));
+        System.out.println(p.shareSameRotation("abcde", "decab"));
+    }
+}
