fd18e35958b4f185b04681e08c34031cef46a3ac
xiaolongwang <xiaolong.wang.dev@gmail.com>
2022-01-12 00:38:57 -0500

    newton iteration

--- src/main/java/math/NewtonIterationFindSquareRoot.java
+++ src/main/java/math/NewtonIterationFindSquareRoot.java
@@ -1,10 +1,10 @@
 package math;
 
 public class NewtonIterationFindSquareRoot {
-    public double sqrt(double n, double precision) {
+    public double sqrt(double n, double epsilon) {
         double x = n;
         double y = x * x - n;
-        while (Math.abs(x * x - n) > precision) {
+        while (Math.abs(-y / (2 * x)) > epsilon) {
             x = -y / (2 * x) + x;
             y = x * x - n;
         }
@@ -13,7 +13,7 @@ public class NewtonIterationFindSquareRoot {
 
     public static void main(String[] args) {
         NewtonIterationFindSquareRoot algo = new NewtonIterationFindSquareRoot();
-        System.out.println(algo.sqrt(10, 0.0001));
+        System.out.println(algo.sqrt(10, 1e-4));
         System.out.println(Math.sqrt(10));
     }
 }
