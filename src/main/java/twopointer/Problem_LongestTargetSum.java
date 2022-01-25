690982982520f13d126f0b2ecb72b6b6f3a29c13
xiaolongwang <xiaolong.wang.dev@gmail.com>
2022-01-24 22:39:48 -0500

    problem: longest target sum v3

--- src/main/java/twopointer/Problem_LongestTargetSum.java
+++ src/main/java/twopointer/Problem_LongestTargetSum.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 public class Problem_LongestTargetSum {
-    public List<Integer> findSimple(int[] input, int target) {
+    public List<Integer> findInAllPositive(int[] input, int target) {
         if (input.length == 0) return null;
         int l = 0;
         int r = 0;
@@ -33,7 +33,7 @@ public class Problem_LongestTargetSum {
         return lResult == null ? null : List.of(lResult, rResult);
     }
 
-    public List<Integer> findMedium(int[] input, int target) {
+    public List<Integer> findInAllInteger(int[] input, int target) {
         if (input.length == 0) return null;
         Map<Integer, Integer> leadingSumIndex = new HashMap<>();
         int leadingSum = 0;
@@ -56,9 +56,61 @@ public class Problem_LongestTargetSum {
         return lResult == null ? null : List.of(lResult, rResult);
     }
 
+    public List<Integer> findNoGreaterThan(int[] input, int target) {
+        int[] smallestSum = new int[input.length];
+        int[] smallestSumEnds = new int[input.length];
+        for (int i = input.length - 1; i >= 0; i--) {
+            if (i + 1 <= input.length - 1) {
+                smallestSum[i] = smallestSum[i + 1] > 0 ? input[i] : input[i] + smallestSum[i + 1];
+                smallestSumEnds[i] = smallestSum[i + 1] > 0 ? i : smallestSumEnds[i + 1];
+            } else {
+                smallestSum[i] = input[i];
+                smallestSumEnds[i] = i;
+            }
+        }
+
+        int starting = 0;
+        int ending = smallestSumEnds[0];
+        int cumulated = smallestSum[0];
+        int lResult = -1;
+        int rResult = -1;
+        if (cumulated <= target) {
+            lResult = starting;
+            rResult = ending;
+        }
+        while (starting < smallestSum.length) {
+            if (ending == smallestSum.length - 1) {
+                // we cannot find a longer one
+                break;
+            }
+            if (cumulated + smallestSum[ending + 1] <= target) {
+                cumulated = cumulated + smallestSum[ending + 1];
+                ending = smallestSumEnds[ending + 1];
+            } else {
+                starting++;
+                if (starting < smallestSum.length) {
+                    cumulated = cumulated - input[starting - 1];
+                    if (starting > ending) {
+                        cumulated = smallestSum[starting];
+                        ending = smallestSumEnds[starting];
+                    }
+                }
+            }
+            if (cumulated <= target && ending - starting > rResult - lResult) {
+                lResult = starting;
+                rResult = ending;
+            }
+        }
+        return List.of(lResult, rResult);
+    }
+
     public static void main(String[] args) {
         Problem_LongestTargetSum p = new Problem_LongestTargetSum();
-        System.out.println(p.findSimple(new int[]{1, 2, 3, 1, 1}, 5));
-        System.out.println(p.findMedium(new int[]{1, -2, 3, -1, -4}, 1));
+        System.out.println(p.findInAllPositive(new int[]{1, 2, 3, 1, 1}, 5));
+        System.out.println(p.findInAllInteger(new int[]{1, -2, 3, -1, -4}, 1));
+        System.out.println(p.findNoGreaterThan(new int[]{1, -2, 3, 1, 10, -4}, 1));
+        System.out.println(p.findNoGreaterThan(new int[]{1, 2, 3, 4}, 1));
+        System.out.println(p.findNoGreaterThan(new int[]{3, 2, 3, 4}, 1));
+        System.out.println(p.findNoGreaterThan(new int[]{3, 2, -10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 1));
     }
 }
