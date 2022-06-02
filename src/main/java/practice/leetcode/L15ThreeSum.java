d949a25a1401df0e8779dbdda6319692cc1b54b7
xiaolongwang <xiaolong.wang.dev@gmail.com>
2022-06-01 22:26:33 -0400

    L1 and L15

---
+++ src/main/java/practice/leetcode/L15ThreeSum.java
@@ -0,0 +1,35 @@
+package practice.leetcode;
+
+import java.util.*;
+
+import static practice.leetcode.L1TwoSum.orderedTwoSumAllUniqueValues;
+
+public class L15ThreeSum {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        Integer prev = null;
+        List<List<Integer>> threeSumResult = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (prev == null || num != prev) {
+                var twoSumResult = orderedTwoSumAllUniqueValues(nums, -num, i + 1);
+                for (var result : twoSumResult) {
+                    result.addFirst(num);
+                    threeSumResult.add(result);
+                }
+            }
+            prev = num;
+        }
+
+        return threeSumResult;
+    }
+
+    public static void main(String[] args) {
+        var q = new L15ThreeSum();
+        var input1 = new int[]{-1, 0, 1, 2, -1, -4};
+        var input2 = new int[]{0, 0, 0, 0};
+        System.out.println(q.threeSum(input1));
+        System.out.println(q.threeSum(input2));
+    }
+}
--- src/main/java/practice/leetcode/L1TwoSum.java
+++ src/main/java/practice/leetcode/L1TwoSum.java
@@ -1,14 +1,15 @@
 package practice.leetcode;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * use hashmap to keep the remainder
  */
 public class L1TwoSum {
-    public int[] solve(int[] nums, int target) {
+    /**
+     * requirement is easy. only find 1 pair
+     */
+    public static int[] solve(int[] nums, int target) {
         // remainder, index
         int[] result = new int[2];
         Map<Integer, Integer> remain = new HashMap<>();
@@ -29,9 +30,69 @@ public class L1TwoSum {
         return result;
     }
 
+    /**
+     * find all pairs of different numbers. This can be used by the three sum problem
+     *
+     * @param nums num array
+     * @param target target sum
+     * @param start where to start trying. It's used by the three sum problem
+     * @return A collection of all pairs. Using linked list for quicker append head operation
+     */
+    public static Collection<LinkedList<Integer>> twoSumAllUniqueValues(int[] nums, int target, int start) {
+        Map<Integer, LinkedList<Integer>> pairs = new HashMap<>();
+
+        Map<Integer, Integer> remain = new HashMap<>();
+        for (int i = start; i < nums.length; i++) {
+            int num = nums[i];
+
+            // check first, so we don't grab the number itself back
+            Integer otherIndex = remain.get(num);
+            if (remain.get(num) != null) {
+                LinkedList<Integer> pair = new LinkedList<>();
+                pair.add(nums[otherIndex]);
+                pair.add(nums[i]);
+                // to prevent a case like: 3, 6, 3 target 9 reporting (3, [3, 6]), (6, [6, 3])
+                if (!pairs.containsKey(target - num)) {
+                    pairs.put(num, pair);
+                }
+            }
+            remain.put(target - num, i);
+        }
+
+        return pairs.values();
+    }
+
+    /**
+     * https://en.wikipedia.org/wiki/3SUM#Quadratic_algorithm
+     * saving the result and avoid dups takes a lot of lines
+     */
+    public static Collection<LinkedList<Integer>> orderedTwoSumAllUniqueValues(int[] nums, int target, int start) {
+        int left = start;
+        int right = nums.length - 1;
+        Map<Integer, LinkedList<Integer>> pairs = new HashMap<>();
+
+        while (left < right) {
+            if (nums[left] + nums[right] == target) {
+                LinkedList<Integer> pair = new LinkedList<>();
+                pair.add(nums[left]);
+                pair.add(nums[right]);
+                pairs.put(nums[left], pair);
+                right--;
+                left++;
+            } else if (nums[left] + nums[right] > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+
+        return pairs.values();
+    }
+
     public static void main(String[] args) {
-        var q = new L1TwoSum();
         var input1 = new int[]{3, 3};
-        System.out.println(Arrays.toString(q.solve(input1, 6)));
+        var input2 = new int[]{3, 3, 6, 6};
+        System.out.println(Arrays.toString(solve(input1, 6)));
+        System.out.println(orderedTwoSumAllUniqueValues(input2, 9, 0));
     }
 }
