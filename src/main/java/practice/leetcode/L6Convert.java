e4137c3a5d05ad3123a0d2ab4c7629f1bcd53e51
xiaolongwang <xiaolong.wang.dev@gmail.com>
2022-06-07 19:31:21 -0400

    L6

--- src/main/java/practice/leetcode/L6Convert.java
+++ src/main/java/practice/leetcode/L6Convert.java
@@ -1,5 +1,9 @@
 package practice.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class L6Convert {
     public static String convert(String s, int numRows) {
         // 0   4   8   12
@@ -18,14 +22,9 @@ public class L6Convert {
         for (int i = 0; i < numRows; i++) {
             int k = 2 * numRows - 2;
             int j = 0;
-            if (i == 0) {
-                while (j * k < s.length()) {
-                    sb.append(s.charAt(j * k));
-                    j++;
-                }
-            } else if (i == numRows - 1) {
-                while (j * k + numRows - 1 < s.length()) {
-                    sb.append(s.charAt(j * k + numRows - 1));
+            if (i == 0 || i == numRows - 1) {
+                while (j * k + i < s.length()) {
+                    sb.append(s.charAt(j * k + i));
                     j++;
                 }
             } else {
@@ -44,9 +43,36 @@ public class L6Convert {
         return sb.toString();
     }
 
+    public static String convertWithExtraSpace(String s, int numRows) {
+        if (numRows == 1) {
+            return s;
+        }
+
+        List<StringBuilder> rows = new ArrayList<>();
+        for (int i = 0; i < Math.min(s.length(), numRows); i++) {
+            rows.add(new StringBuilder());
+        }
+
+        int currentRow = 0;
+        boolean goingDown = true;
+
+        for (int i = 0; i < s.length(); i++) {
+            rows.get(currentRow).append(s.charAt(i));
+            if (i != 0 && (currentRow == 0 || currentRow == numRows - 1)) {
+                goingDown = !goingDown;
+            }
+            currentRow = goingDown ? currentRow + 1 : currentRow - 1;
+        }
+
+        return rows.stream().map(StringBuilder::toString).collect(Collectors.joining());
+    }
+
     public static void main(String[] args) {
-//        System.out.println(convert("PAYPALISHIRING", 3));
-//        System.out.println(convert("PAYPALISHIRING", 4));
+        System.out.println(convert("PAYPALISHIRING", 3));
+        System.out.println(convert("PAYPALISHIRING", 4));
         System.out.println(convert("AB", 2));
+        System.out.println(convertWithExtraSpace("PAYPALISHIRING", 3));
+        System.out.println(convertWithExtraSpace("PAYPALISHIRING", 4));
+        System.out.println(convertWithExtraSpace("AB", 2));
     }
 }
