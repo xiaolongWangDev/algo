2ce939196ae60913b6cb7e79d5149fc9c74267ae
xiaolongwang <xiaolong.wang.dev@gmail.com>
2022-08-18 20:43:48 -0700

    Insert into sorted loop

--- src/main/java/practice/zuosbook/chapter2/InsertIntoSortedLoop.java
+++ src/main/java/practice/zuosbook/chapter2/InsertIntoSortedLoop.java
@@ -5,25 +5,14 @@ import helper.ListUtils;
 
 public class InsertIntoSortedLoop {
     public static ListNode solve(ListNode head, int num) {
-        ListNode cur = head;
-        while (cur.next != head) {
-            cur = cur.next;
-        }
-        ListNode prev = cur;
-        cur = head;
 
-        // case that the head is changed
-        if (head.val > num) {
-            ListNode newNode = new ListNode();
-            newNode.val = num;
-            newNode.next = cur;
-            prev.next = newNode;
-            return newNode;
-        }
+        ListNode prev = head;
+        ListNode cur = head.next;
 
-        prev = prev.next;
-        cur = cur.next;
-        while (cur != head && cur.val <= num) {
+        while (cur != head) {
+            if (prev.val < num && num <= cur.val) {
+                break;
+            }
             prev = cur;
             cur = cur.next;
         }
@@ -32,15 +21,16 @@ public class InsertIntoSortedLoop {
         newNode.val = num;
         newNode.next = cur;
         prev.next = newNode;
-        return head;
+        return head.val > num ? newNode : head;
     }
 
     public static void main(String[] args) {
         ListNode head = ListUtils.create(new int[]{1, 2, 2, 3, 5, 6, 7});
         head.next.next.next.next.next.next.next = head;
 
-        ListUtils.printFirstN(solve(head, -1), 8);
+//        ListUtils.printFirstN(solve(head, -1), 8);
 //        ListUtils.printFirstN(solve(head, 4), 8);
+        ListUtils.printFirstN(solve(head, 5), 8);
 //        ListUtils.printFirstN(solve(head, 11), 8);
     }
 }
